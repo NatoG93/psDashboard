a576cee59a4303783d5cdfcc8b8e6fb4dcf161bf
Sergio Guerra <sergiguerra93@gmail.com>
2024-09-30 21:11:04 -0600

    Update

--- dashv1.py
+++ dashv1.py
@@ -10,7 +10,7 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="PS Dash",page_icon=":bar_chart:",layout='wide')
 st.title(" :bar_chart: ProShippiers Month Over Month Dashboard")
 
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True) # Remove the space from top to content
+# st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True) # Remove the space from top to content
 
 fl = st.file_uploader(":file_folder: Upload CSV file", type=['csv',"xls","xlsx"])
 if fl is not None:
@@ -24,3 +24,13 @@ else:
 
 st.sidebar.header("Filtrar datos:")
 month = st.sidebar.multiselect("Seleccionar mes:",df["Month"].unique())
+broker = st.sidebar.multiselect("Seleccionar broker:",df["Broker"].unique())
+
+if not month and not broker:
+    filtered = df
+elif not month:
+    filtered = df[df['Broker'].isin(broker)]
+elif not broker:
+    filtered = df[df['Month'].isin(month)]
+else:
+    filtered = df[df['Month'].isin(month)]
